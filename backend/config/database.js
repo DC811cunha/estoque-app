06bcf6c5a8f7f349327929bcdaa0d28c56b21a34
Diego Cunha <dc811cunha@gmail.com>
2025-03-12 20:43:56 -0300

    Exclusao de dados sensiveis

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,   // Nome do banco (ex.: "estoque")
-  process.env.DB_USER,   // Usuário (ex.: "root")
-  process.env.DB_PASS,   // Senha (ex.: "DC811roo&")
+  process.env.DB_USER,   // Usuário
+  process.env.DB_PASS,   // Senha
   {
     host: process.env.DB_HOST, // Ex.: "localhost"
     dialect: 'mysql',
